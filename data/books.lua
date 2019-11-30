913c2da6b7aefdbf558fb8226f4a78f58ff27ff1
William Adams <Wiladams@users.noreply.github.com>
2019-11-30 09:05:53 -0800

    create book data set

---
+++ data/books.lua
@@ -0,0 +1,154 @@
+local books = {
+    {
+        isbn = "0-13-086985-6",
+        title = "Windows Graphics Programming",
+        subtitle = "Win32 GDI and DirectDraw",
+        authors = {"Feng Yuan"},
+        publisher = "Hewlett-Packard Professional Books"
+        online = "",
+        notes = "CD Included"
+    },
+
+    {
+        isbn = "978-0-9981770-1-4",
+        title = "Mentoring Matters",
+        subtitle = "A Practical Guide to Learning-Focused Relationships",
+        edition = "third edition",
+        authors = {"Lauren Lipton", "Bruce Wellman"},
+        publisher = "MiraVia",
+        online = "",
+        notes = ""
+    },
+
+    {
+        isbn = "978-1-63186-161-1",
+        title = "Cabinets, Vanities, and Countertops",
+        subtitle = "",
+        edition = "",
+        series = "Taunton's For Pros by Pros",
+        authors = {"Fine Homebuilding"},
+        publisher = "The Taunton Press",
+        online = "",
+        notes = ""
+    },
+
+    {
+        isbn = "1-56881-137-3",
+        title = "Geometric Modeling with Splines",
+        subtitle = "an Introduction",
+        edition = "",
+        series = "",
+        authors = {"Elaine Cohen","Richard F. Riesenfeld","Gershon Elber"},
+        publisher = "",
+        online = "",
+        notes = ""
+    },
+
+    {
+        isbn = "1-55860-874-5",
+        isbn13 = "978-1-55860-874-0",
+        title = "ARM System Developer's Guide",
+        subtitle = "Designing and Optimizing System Software",
+        edition = "",
+        series = "Elsevier",
+        authors = {"Andrew N. Sloss","Dominic Symes", "Chris Wright"},
+        publisher = "Morgan Kaufman",
+        online = "",
+        notes = ""
+    },
+
+    {
+        isbn = "978-1-84753-696-9",
+        title = "ARM Assembly Language",
+        subtitle = "an Introduction",
+        edition = "",
+        series = "",
+        authors = {"J.R. Gibson"},
+        publisher = "",
+        online = "",
+        notes = ""
+        tags = ""
+    },
+
+    {
+        isbn = "978-0-8348-1516-2",
+        title = "The Art of Japanese Joinery",
+        subtitle = "",
+        edition = "",
+        series = "",
+        authors = {"Kiyosi Seike"},
+        publisher = "",
+        online = "",
+        notes = ""
+        tags = ""
+    },
+
+    {
+        isbn = "0-201-67519-6",
+        title = "ARM system-on-chip architecture",
+        subtitle = "",
+        edition = "second edition",
+        series = "",
+        authors = {"steve furber"},
+        publisher = "Addison Wesley",
+        online = "",
+        notes = ""
+        tags = ""
+    },
+
+    {
+        isbn = "0-596-10242-9",
+        isbn13 = "978-0-596-10242-5",
+        title = "Fonts and Encodings",
+        subtitle = "",
+        edition = "",
+        series = "",
+        authors = {"Yannis Haralambous"},
+        publisher = "O'Reilly",
+        online = "",
+        notes = "Translated by P.Scott Horne"
+        tags = ""
+    },
+
+    {
+        isbn = "0-596-00403-6",
+        title = "Kerberos",
+        subtitle = "The Definitive Guide",
+        edition = "",
+        year = "",
+        series = "",
+        authors = {""},
+        publisher = "O'Reilly",
+        online = "",
+        notes = ""
+        tags = "computer, identity, networking, security"
+    },
+
+    {
+        isbn = "0-07-048677-8",
+        title = "Mathematical Elements for Computer Graphics",
+        subtitle = "",
+        edition = "second edition",
+        year = "",
+        series = "",
+        authors = {"David F. Rogers", "J. Alan Adams"},
+        publisher = "TATA McGraw-Hill",
+        online = "",
+        notes = ""
+        tags = ""
+    },
+
+    {
+        isbn = "",
+        title = "",
+        subtitle = "",
+        edition = "",
+        year = "",
+        series = "",
+        authors = {""},
+        publisher = "",
+        online = "",
+        notes = ""
+        tags = ""
+    },
+}
